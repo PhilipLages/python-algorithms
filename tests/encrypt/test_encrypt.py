c8fe0b7e10e136418fafb5b8894b0170dd568ced
PhilipLages <psilvaelages@gmail.com>
2023-04-28 12:07:00 -0300

    feat: Implements test_encrypt_message test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+
+    # Test valid inputs with odd key
+    assert encrypt_message("hello world", 5) == "olleh_dlrow "
+
+    # Test valid inputs with even key
+    assert encrypt_message("hello world", 6) == "dlrow_ olleh"
+
+    # Test invalid key type
+    with raises(TypeError):
+        encrypt_message("hello world", "key")
+
+    # Test invalid message type
+    with raises(TypeError):
+        encrypt_message(1234, 5)
+
+    # Test invalid key value
+    assert encrypt_message("hello world", 0) == "dlrow olleh"
+    assert encrypt_message("hello world", 100) == "dlrow olleh"
+
+
