93e0dd34432871f210360ebca5f055db9cab45b1
PhilipLages <psilvaelages@gmail.com>
2023-04-28 11:45:23 -0300

    feat: Implements study_schedule
    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui.!"""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple], target_time: int) -> int:
+    if not isinstance(target_time, int):
+        return None
+    
+    if not all(isinstance(period[0], int) and isinstance(period[1], int) for period in permanence_period):
+        return None
+    
+    people_counter = sum(1 for period in permanence_period if target_time in range(period[0], period[1] + 1))
+    return people_counter
+
+
