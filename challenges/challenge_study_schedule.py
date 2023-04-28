879d267b512775f5cbf2ff12fbcf81f9045c48cc
PhilipLages <psilvaelages@gmail.com>
2023-04-28 11:52:53 -0300

    refactor: Uses black to fix flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,16 @@
 def study_schedule(permanence_period: list[tuple], target_time: int) -> int:
     if not isinstance(target_time, int):
         return None
-    
+
     if not all(
-            isinstance(period[0], int) and isinstance(period[1], int) 
-                for period in permanence_period
-        ):
+        isinstance(period[0], int) and isinstance(period[1], int)
+        for period in permanence_period
+    ):
         return None
-    
+
     people_counter = sum(
-            1 for period in permanence_period 
-                if target_time in range(period[0], period[1] + 1)
-        )
+        1
+        for period in permanence_period
+        if target_time in range(period[0], period[1] + 1)
+    )
     return people_counter
-
-
