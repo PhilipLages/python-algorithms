6bb9f45076e351891a620482960b0108f3ff2ae8
PhilipLages <psilvaelages@gmail.com>
2023-05-03 18:43:55 -0300

    feat: Implements is palindrome iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    left = 0
+    right = len(word) - 1
+
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True if len(word) > 0 else False
